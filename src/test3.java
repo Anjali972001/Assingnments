ac66e9fc9fafacf87a4b261401b21b5e10116918
Anjali <anjalikumari972001@gmail.com>
2021-11-14 21:51:55 +0530

    practice

---
+++ src/test3.java
@@ -0,0 +1,17 @@
+public class test3 {
+    void show(Object a)
+    {
+        System.out.println("Object method.");
+    }
+    void show (String b)
+    {
+        System.out.println("String method.");
+    }
+
+    public static void main(String[] args) {
+        test3 ttt = new test3();
+        ttt.show('a');// object is the parent class of all the classes in java.
+        ttt.show("anjali");
+        // output :- Object method and string method
+    }
+}
